470ed11364fc8d913a6fcb52deb36ebf0b02b4c1
Deha Efecan Süer <dehasuer97@gmail.com>
2022-05-30 19:11:56 +0300

    javascript file added

--- day01/drum.js
+++ day01/drum.js
@@ -1,12 +1,18 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //to catch the audio
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //to catch the key
-    if(!audio) return; //to stop the function
-    audio.currentTime = 0; //to repeat the audio
-    audio.play(); //to play the audio
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-    
-});
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
 
-
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);

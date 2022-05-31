c730ae48d666b42e00a948c9c9619bf24e57705a
Deha Efecan Süer <dehasuer97@gmail.com>
2022-05-31 11:28:55 +0300

    clock app updated

--- day02/clock.js
+++ day02/clock.js
@@ -2,13 +2,17 @@ const hour = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const sec = document.querySelector('.sec');
 
-const updateTime = function(){
-    const newTime = new Date()
-    hour.innerHTML = newTime.getHours();
-    min.innerHTML = newTime.getMinutes();
-    sec.innerHTML = newTime.getSeconds();
+const setDate = function(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    sec.style.transform = `rotate(${(seconds / 60)* 360 + 90}deg)`;
+
+    const mins = now.getMinutes();
+    min.style.transform = `rotate(${(mins / 60)* 360 + 90}deg)`;
+
+    const hours = now.getHours();
+    hour.style.transform = `rotate(${(hours / 12)* 360 + 90}deg)`;
+
+    console.log(hours);
 }
-
-
-setInterval(updateTime, 1000);
-
+setInterval(setDate, 1000);
